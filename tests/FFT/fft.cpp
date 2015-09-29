ec4a81e4e28f3575b5c2f8019e7c498758a11c4d
akke <akkeaccu@msn.com>
2015-09-29 14:12:57 +0200

    directory analysis->stft

--- tests/FFT/fft.cpp
+++ tests/FFT/fft.cpp
@@ -7,8 +7,14 @@
 
 */
 
-
+#define TWOPI (M_PI+M_PI)
+#define FS 44100
 #define ARGCOUNT 1
+
+double imabs(double re, double im) {
+    double mag = std::sqrt((re*re)+(im*im));
+    return mag;
+}
 
 double* append(double* a, unsigned long Na, double* b, unsigned long Nb) {  // change to generic
     double* y = new double[Na + Nb];
@@ -22,9 +28,9 @@ double* append(double* a, unsigned long Na, double* b, unsigned long Nb) {  // c
 };
 
 double* scramble(double* x, unsigned long N) {   // change to generic function!
-    N = N >> 2;
+    N = N >> 1;
     
-    if(N > 0) {
+    if(N > 1) {
         double* even = new double[N];
         double* oneven = new double[N];
         for(unsigned long n=0; n<N; ++n) {
@@ -60,57 +66,50 @@ int dFFT(double* x_re, double* x_im, unsigned long N, double* X_re, double* X_im
         x_im = zeropad(x_im, N, newN);
         N = newN;
     };
- 
+    unsigned long depth = log2(N);
+    unsigned long tempN;
+
     // rearrange input buffers:
     double* re = scramble(x_re, N);
     double* im = scramble(x_im, N);
 
+    // declare arrays:
+    double* w_re = new double[depth];
+    double* w_im = new double[depth];
+    double* copy_re = new double[N];
+    double* copy_im = new double[N];
 
     // FFT:
-    double a, b, c, d, oneven_re, oneven_im;
-    double** swap_re = new double*[2];
-    double** swap_im = new double*[2];
-    for(int i=0; i<2; ++i) {
-        swap_re[i] = new double[N];
-        swap_im[i] = new double[N];   
-    }
-    int swap_w_id = 1;
-    unsigned long tempN = N >> 2;
-    for(unsigned long k=0; k<N >> 2; ++k) {
-        // copy buffers:
-        for(unsigned long n=0; n<N; ++n) {  
-            swap_re[0][n] = x_re[n];
-            swap_im[0][n] = x_im[n];
-        }
+    for(unsigned long k=0; k<N >> 1; ++k) {
+        // calculate twiddle factors
+         for(unsigned long d=0; d<depth; ++d) {
+            w_re[d] = std::cos(TWOPI * k * pow(2, d) / (long double)N);
+            w_im[d] = -std::sin(TWOPI * k * pow(2, d) / (long double)N);
+        }        
+        // copy inputbuffers
+        for(unsigned long n=0; n<N; ++n) {
+            copy_re[n] = re[n];
+            copy_im[n] = im[n];
+        };       
+
+        tempN = N;
+        // the FFT:
+
         for(unsigned long d=0; d<depth; ++d) {
-            
             for(unsigned long n=0; n<tempN; ++n) {
-                // a + bj * c + dj = ac - db + adj + cbj
-                a = swap_re[swap_r_id][2*n + 1];    // get data points
-                b = swap_im[swap_r_id][2*n + 1];
-                c = twiddle_re[depth - d - 1];      // load twiddle factor
-                d = twiddle_im[depth - d - 1];
-                oneven_re = a*c - d*b;
-                oneven_im = a*d + c*b;
-                swap_re[swap_w_id] = swap_re[swap_r_id][2*n] + oneven_re;
-                swap_im[swap_w_id] = swap_im[swap_r_id][2*n] + oneven_im;
-
+                // a+bj * c+dj = a*c - b*d + adj * cbj
+                copy_re[2*n] += copy_re[2*n + 1] * w_re[depth - d - 1] - copy_im[2*n + 1] * w_im[depth - d - 1];
+                copy_im[2*n] += copy_re[2*n + 1] * w_im[depth - d - 1] + copy_im[2*n + 1] * w_re[depth - d - 1];
             };
-            swap_w_id = (swap_w_id + 1) % 2;
-            swap_r_id = (swap_w_id + 1) % 2;
-            tempN = N >> 2;
-        }; 
-
-
+            tempN = tempN >> 1;
+        };
+        
+       X_re[k] = copy_re[0];
+       X_re[N - k - 1] = copy_re[0];
+       X_im[k] = copy_im[0];
+       X_im[N - k - 1] = copy_im[0];
     };
-    // calculate twiddle factors:
-    unsigned int depth = log2(N);   // get depth   
-    double* twiddle = new double[depth];
-    for(unsigned long d=0; d<depth; ++d) 
-        twiddle     
-    
-
-    return 0;
+   return 0;
 };
 
 
@@ -119,14 +118,24 @@ int main(int argc, char** argv) {
     if(argc < ARGCOUNT) {
         return -1;
     }
-    unsigned long N = atoi(argv[1]);
+    unsigned long N = pow(2, atoi(argv[1]));
 
     double* x_re = new double[N];
     double* x_im = new double[N];
     double* X_re = new double[N];
     double* X_im = new double[N];
     
+    std::cout << "fill input buffers\n"; 
+    for(unsigned long n=0; n<N; ++n) {
+        x_re[n] = std::sin(TWOPI * 2 * n / N);
+        x_im[n] = 0;
+    };
 
+    std::cout << "calc FFT\n";
     dFFT(x_re, x_im, N, X_re, X_im);
 
+
+    for(unsigned long k=0; k<N >> 1; ++k) {
+        std::cout << "bin: " << k << "\tmag: " << imabs(X_re[k], X_im[k]) << std::endl;
+    };
 };
