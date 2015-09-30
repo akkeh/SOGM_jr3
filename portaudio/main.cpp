7c574680cbdc8fe73c1822e9dc2dc600f7fe2612
Olaf Wisselink <olafwisselink@live.nl>
2015-09-30 15:03:29 +0200

    portaudio

--- portaudio/main.cpp
+++ portaudio/main.cpp
@@ -1,15 +1,28 @@
-#include "audioIO.h"
+#include "audio_io.h"
+
+#define SAMPLERATE	44100
+#define CHANNELS	1
+#define FRAMES	    1024
 
 int main() {
+    Audio_IO audioStream(SAMPLERATE,CHANNELS);
+    float* buffer = new float[FRAMES*CHANNELS];
+    static int input_device;
 
-  audioIO audioIn;
-  audioIn.startAudio();
+    audioStream.set_mode(AUDIO_IO_READONLY);
+    audioStream.set_framesperbuffer(FRAMES);
+    audioStream.initialise();
+    audioStream.list_devices();
+    std::cout << "\nGive input device number: ";
+    std::cin >> input_device;
+    audioStream.set_input_device(input_device);
+    audioStream.start_server();
 
-/*
-   while (true) {
-     audioIn.readSamples();
-     audioIn.writeSamples();
-   }
-*/
-  return 0;
+    while (true) {
+        audioStream.read(buffer);
+    }
+
+    delete[] buffer;
+
+    return 0;
 }
