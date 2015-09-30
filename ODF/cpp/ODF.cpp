1cc3b27f2cbcc54615eaad0f311772e9c103080b
akke <akkeaccu@msn.com>
2015-09-30 14:04:46 +0200

    fixed mem allocation in window function

--- ODF/cpp/ODF.cpp
+++ ODF/cpp/ODF.cpp
@@ -67,7 +67,7 @@ double* getBlackman(unsigned long M) {
     a1 = 9240.0/18608.0;
     a2 = 1430.0/18608.0;
 
-    double* w = new double(M);
+    double* w = new double[M];
     for(unsigned long n=0; n<M; ++n) {
         w[n] = a0 - a1 * std::cos((TWOPI * (long double)n) / ((long double)M - 1)) + a2 * std::cos((FOURPI * (long double)n) / ((long double)M - 1));
     };
@@ -89,22 +89,18 @@ double* zeropad(double* x, unsigned long N, unsigned long newN) {
 
 double** STFT(double* x, unsigned long N, unsigned long M, unsigned long bins, unsigned long H) {
     // N multiple of M?
-    float diff = (long double)N/(long double)M - (unsigned long)((long double)N/(long double)M);
-    unsigned long newN = N+(M*diff);
-    double* x_zp = zeropad(x, N, newN);
+    unsigned long frames = N/H;    
 
-    unsigned long frames = newN/H;
     double** X = new double*[frames];
-    for(unsigned long l=0; l<frames; l++)
-        X[l] = new double[bins*2];
+    
     double* w = getBlackman(M);
 
     double* x_part = new double[bins*2];
     for(unsigned long l=0; l<frames; l++) {
         // copy input buffer:
         for(unsigned long n=0; n<M; n++) {
-            x_part[2*n] = x[H*l + 2*n] * w[n];          // even
-            x_part[2*n + 1] = x[H*l + 2*n + 1] * w[n];  // oneven
+            x_part[2*n] = x[(2*H)*l + 2*n] * w[n];          // even
+            x_part[2*n + 1] = x[(2*H)*l + 2*n + 1] * w[n];  // oneven
         };
         X[l] = zeropad(x_part, M*2, bins*2);
         FFT(X[l], bins);
@@ -126,54 +122,19 @@ int main() {
     };
     unsigned long M = 512;
     unsigned long H = 128;
-    unsigned long bins = M;//1024;
+    unsigned long bins = 1024;
     double** X = STFT(x, N, M, bins, H);
 
-    for (unsigned long m=0; m<1; ++m) {
+    for (unsigned long m=0; m<N/H; ++m) {
         //std::cout << "frame: " << m << std::endl;
         for (unsigned long k=0; k<bins; ++k) {
             std::cout << "\t" << imabs(X[m][2*k], X[m][2*k + 1]) << std::endl;
         }
     }
 
-    for (unsigned long m=0; m<N/H; ++m) {
+    for (unsigned long m=0; m<N/H - 1; ++m) {
         delete[] X[m];
     }
 };
 
-/*
-int STFT(double* x, double** outX, unsigned long NN, unsigned long bins, unsigned long M, unsigned long H, double th, double inhibTh, unsigned long inhibRel) {
 
-        -
-        -   assumes x is normalised!
-
-
-    unsigned long N = NN >> 1;
-
-    double* win = getBlackman(M);   // calculate window
-    unsigned long frames = N/H;
-    double** X = new double*[frames];
-
-    double* x_part = new double[bins*2];
-
-    for(unsigned long m=0; m<frames; m++) {
-        std::cout << "1\n";
-        X[m] = new double[2*bins];
-        std::cout << "2\n";
-        // copy signal and apply window
-        for(unsigned long n=0; n<M; ++n) {
-            x_part[2*n] = x[m*H + 2*n] * win[n];
-            x_part[2*n + 1] = x[m*H + 2*n + 1] * win[n];
-        };
-        x_part = zeropad(x_part, M*2, bins*2);
-        FFT(x_part, bins);
-        X[m] = x_part;
-    };
-
-    //delete[] win;
-    return 0;
-
-};
-
-
-*/
